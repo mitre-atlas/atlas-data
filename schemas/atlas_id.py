debc1ca10be15a79a9b09e73663749b59c59627a
dnsouli <dnsouli@mitre.org>
2022-05-04 19:48:28 +0000

    Changed ID requirements

--- schemas/atlas_id.py
+++ schemas/atlas_id.py
@@ -3,10 +3,10 @@ from schema import Regex, Schema
 """Describes ATLAS ID schemas."""
 
 # Constants for ID formats
-TACTIC_ID_PATTERN       = r'AML\.TA\d{4}'         # AML.TA0000
-TECHNIQUE_ID_PATTERN    = r'AML\.T\d{4}'          # AML.T0000
-SUBTECHNIQUE_ID_PATTERN = r'AML\.T\d{4}\.\d{3}'   # AML.T0000.000
-CASE_STUDY_ID_PATTERN   = r'AML\.CS\d{4}'         # AML.CS0000
+TACTIC_ID_PATTERN       = r'\w{1,}|\d{1,}(\.\w{1,}|\d{1,})?\.TA\d{4}'         # AML.TA0000 || AML.ABC123.TA0000 || AML123.TA0000
+TECHNIQUE_ID_PATTERN    = r'\w{1,}|\d{1,}(\.\w{1,}|\d{1,})?\.T\d{4}'          # AML.T0000 || AML.ABC123.T0000 || AML123.T0000
+SUBTECHNIQUE_ID_PATTERN = r'\w{1,}|\d{1,}(\.\w{1,}|\d{1,})?\.T\d{4}\.\d{3}'   # AML.T0000.000 || AML.ABC123.T0000.00 || AML123.T0000.00
+CASE_STUDY_ID_PATTERN   = r'\w{1,}|\d{1,}(\.\w{1,}|\d{1,})?\.CS\d{4}'         # AML.CS0000 || AML.ABC123.CS0000 || AML123.CS0000
 
 # Exact match patterns for the above, in Schema form
 TACTIC_ID_REGEX_EXACT = Schema(
